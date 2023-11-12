eb75ab38307aa1345b2f9240e766ad7563352ecf
caozuoheng <1628512141@qq.com>
2023-11-12 16:54:30 +0800

    '响应式布局'

--- components/TitleBar.vue
+++ components/TitleBar.vue
@@ -4,37 +4,39 @@
 
 <template>
   <header>
-    <div class="title">
-      <img class="tooth" src="../src/assets/tooth.png">
-      <span>
-       <h3 style="margin-bottom: 10px;margin-top: 8px">口腔检查系统</h3>
-       <h4 style="margin: 0">Oral examination system</h4>
-      </span>
+    <div class='title'>
+      <img class='tooth' src='../src/assets/tooth.png'>
+      <div >
+        <h3 style='margin-bottom: 2vh;height: 4vh;overflow: hidden'>口腔检查系统</h3>
+        <h4 style='margin: 0;height: 2vh;overflow: hidden'>Oral examination system</h4>
+      </div>
     </div>
     <slot>
-      <div class="user">
-      <span style="text-align: right;">
-       <h4 style="margin-top: 15px;margin-bottom: 0">刘强</h4>
-       <p style="margin: 0;font-size: 10px;color: #a2a1a7">口腔医科-主任医师</p>
+      <div class='user'>
+      <span style='text-align: right;'>
+       <h4 style='margin-top: 15px;margin-bottom: 0'>刘强</h4>
+       <p style='margin: 0;font-size: 10px;color: #a2a1a7'>口腔医科-主任医师</p>
       </span>
-        <el-avatar class="user-avatar" size="200" style="float:right;" shape="circle" src="src/assets/imgs/avatar.png"
+        <el-avatar class='user-avatar' size='200' style='float:right;' shape='circle' src='src/assets/imgs/avatar.png'
         />
       </div>
     </slot>
   </header>
 </template>
 
-<style scoped lang="scss">
+<style scoped lang='scss'>
 .tooth {
   float: left;
-  width: 60px;
-  height: 65px;
+  width: 6vw;
+  height: 9vh;
   object-fit: scale-down;
   vertical-align: middle;
 }
+
 .user-avatar {
   margin: 12px;
 }
+
 header {
   position: relative;
   width: 100%;
@@ -43,20 +45,41 @@ header {
   background-color: #20252a;
   height: 12%;
 }
+
 .user span {
   display: inline-block;
   vertical-align: middle;
 }
 
-.title span {
+.title div {
   display: inline-block;
   vertical-align: middle;
+  height: 10px;
 }
 
-.title span h3 {
+.title div h3 {
+  font-size: 1.5vw;
   margin: 0;
   border: 0;
 }
+
+.title div h4 {
+  font-size: 1vw;
+  margin: 0;
+  border: 0;
+}
+
+@media screen and (max-width: 700px) {
+  .title div h3 {
+    font-size: 3vw;
+    margin: 0;
+    border: 0;
+  }
+  .title div h4 {
+    font-size: 2vw;
+  }
+}
+
 .title {
   margin-top: 10px;
   margin-bottom: 10px;
