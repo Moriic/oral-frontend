67fbfad8ff9358043e0a2cede762ee65958ab51c
yulnn <2280014437@qq.com>
2023-12-04 13:34:08 +0800

    完善医生患者页面

--- src/api/patient.js
+++ src/api/patient.js
@@ -3,5 +3,7 @@ export const patienthandleAdd = (data) => request.post('/admin/register', data)
 export const patienthandleEdit = (data) => request.put('/admin/patient', data)
 export const patienthandleDelete = (id) => request.DELETE('/admin/register', id)
 export const patientGetinfo = ({ page, pagesize }) =>
-  request.GET('/admin/register/page', { page, pagesize })
+  request.get('/admin/register/page?page=${page}&pageSize=${pageSize}')
 export const patientSearchinfo = (id) => request.GET('/admin/register/{id}', id)
+
+ 
